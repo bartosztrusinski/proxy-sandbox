83cad38365afff0de02877cf9ccb0037096e7112
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-08-07 00:34:25 +0200

    create no undefined values proxy

---
+++ src/proxies/noUndefinedProxy.ts
@@ -0,0 +1,34 @@
+const noUndefinedProxy = <T extends object>(target: T): T =>
+  new Proxy(target, {
+    get(target, prop) {
+      if (!(prop in target)) {
+        throw new Error(`Property ${String(prop)} does not exist`);
+      }
+
+      return Reflect.get(target, prop);
+    },
+  });
+
+type Car = {
+  brand: string;
+  model: string;
+  engine: string;
+  horsepower: number;
+};
+
+let car: Car = {
+  brand: 'Nissan',
+  model: 'Skyline',
+  engine: 'RB26',
+  horsepower: 276,
+};
+
+car = noUndefinedProxy(car);
+
+try {
+  console.log(car.brand);
+  // @ts-expect-error: year is undefined - proxy throws an error
+  console.log(car.year);
+} catch (error) {
+  console.error(error instanceof Error ? error.message : 'An error occurred');
+}
